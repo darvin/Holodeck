184f079b9e3c2894205d67cf9c7814d6042e334f
STC <standardtemplateconstruct2022@gmail.com>
2023-04-03 02:13:52 -0400

    x

---
+++ holodeck/gpt_image.py
@@ -0,0 +1,13 @@
+from diffusers import StableDiffusionPipeline
+import torch
+
+model_id = "Envvi/Inkpunk-Diffusion"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+def generate_image(prompt):
+    return pipe(prompt).images[0]
+ 
+ 
+if __name__=="__main__":
+    print(generate_image('The entire Cobalt Building of Machines is visible. Machines with intricate cogs and tubes, steampunk pipes, and colorful cobalt energy coursing through them on background of the bustling futuristic city with flying cars during the afternoon with warm lighting. nvinkpunk'))
