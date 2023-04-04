56df255c840638feb8ed8f13db7dd8f26a506911
stc <standardtemplateconstruct2022@gmail.com>
2023-04-05 01:11:38 +0300

    imagesplitter added

--- holodeck/gpt_image.py
+++ holodeck/gpt_image.py
@@ -15,7 +15,7 @@ async def generate_image(prompt):
 
     try:
         # Route Generate Image
-        api_response = await api_instance.route_generate_image_verified_image_verified_get(prompt, api_token, max_attempts=2)
+        api_response = await api_instance.route_generate_image_verified_image_verified_get(prompt, api_token, max_attempts=8)
         return api_response
     except Exception as e:
         print("Exception when calling DefaultApi->route_generate_image_image_get: %s\n" % e)
