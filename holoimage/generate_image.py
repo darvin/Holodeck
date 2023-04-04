403116df263ada2d3cc1f831cb82ac0c75298a86
STC <standardtemplateconstruct2022@gmail.com>
2023-04-04 11:09:23 -0400

    image verification wip

---
+++ holoimage/generate_image.py
@@ -0,0 +1,44 @@
+from transformers import pipeline
+from PIL import Image
+import torch
+import numpy as np
+from io import BytesIO
+from diffusers import StableDiffusionPipeline
+import torch
+from verify_image import verify_image
+
+async def generate_image(prompt):
+    return pipe(prompt).images[0]
+
+
+ 
+
+def convert_image_to_byte_io(image):
+    output_image = Image.fromarray(np.uint8(image)).convert('RGB')
+
+    byte_io = BytesIO()
+    output_image.save(byte_io, format='PNG')
+    byte_io.seek(0)
+    return byte_io
+
+
+async def generate_image_verified_byte_io(prompt, max_attempts):
+    image = await generate_image(prompt)
+    is_good = await verify_image(image, prompt)
+    return convert_image_to_byte_io(image)
+
+async def generate_image_byte_io(prompt):
+    output = await generate_image(prompt)
+    return convert_image_to_byte_io(output)
+
+def init_generate_image():
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipe.safety_checker = lambda images, **kwargs: (images, [False] * len(images))
+    pipe = pipe.to("cuda")
+    return pipe
+
+
+# model_id = "prompthero/openjourney-v4"
+model_id = "Envvi/Inkpunk-Diffusion"
+# model_id = "runwayml/stable-diffusion-v1-5"
+pipe = init_generate_image()
---
+++ holoimage/verify_image.py
@@ -0,0 +1,44 @@
+from transformers import pipeline
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+
+from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path, verbose=True)
+
+prompt_image_verify = PromptTemplate(
+    input_variables=["image_description", "prompt"],
+    template="""
+image is described as following: {image_description} 
+    
+desired image is such: "{prompt}"
+
+verify that this is good image for whats requested
+
+be extra critical! 
+
+respond with "YES" or "NO"
+""")
+
+
+
+
+async def verify_image(image, prompt):
+    image_description = pipe_description(image)[0]['generated_text']
+    print(image_description)
+    is_good_txt = chain_image_verify({'image_description':image_description, 'prompt': prompt})['text'].lower()
+    print(is_good_txt)
+    is_good = "yes" in is_good_txt or "good" in is_good_txt
+    return is_good
+
+def initialize_verify_image():
+    llm = OpenAI(temperature=0.9)
+    chain_image_verify = LLMChain(llm=llm, prompt=prompt_image_verify)
+    pipe = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+    return (chain_image_verify, pipe)
+
+
+chain_image_verify, pipe_description = initialize_verify_image()
