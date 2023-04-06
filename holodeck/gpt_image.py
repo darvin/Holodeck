34dccd2dae1c9c2365dafb13cd3b171dc7a52cdd
stc <standardtemplateconstruct2022@gmail.com>
2023-04-06 20:06:24 +0300

    generating images from hosted hugging face api

--- holodeck/gpt_image.py
+++ holodeck/gpt_image.py
@@ -4,11 +4,11 @@ from .holoimage_client import AsyncApis, ApiClient
 from pprint import pprint
 import os
 from .settings import *
+import requests
+import io
 
 
-
-
-async def generate_image(prompt, negative_prompt):
+async def generate_image_holoimage(prompt, negative_prompt):
     # Create an instance of the API class
     api_instance = AsyncApis(ApiClient(os.environ.get('HOLOIMAGE_API_URL'))).default_api
     api_token = os.environ.get('HOLOIMAGE_API_TOKEN')
@@ -20,7 +20,23 @@ async def generate_image(prompt, negative_prompt):
     except Exception as e:
         print("Exception when calling DefaultApi->route_generate_image_image_get: %s\n" % e)
 
- 
- 
+async def generate_image_huggingface(prompt):
+    API_URL = "https://api-inference.huggingface.co/models/Envvi/Inkpunk-Diffusion"
+    api_token = os.environ.get('HUGGINGFACE_API_KEY')
+
+    headers = {"Authorization": f"Bearer {api_token}"}
+
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.content
+    image_bytes = query({
+        "inputs": prompt,
+    })
+    return io.BytesIO(image_bytes)
+
+async def generate_image(prompt, negative_prompt):
+    # return await generate_image_holoimage(prompt, negative_prompt)
+    return await generate_image_huggingface(prompt)
+
 if __name__=="__main__":
     print(generate_image('The entire Cobalt Building of Machines is visible. Machines with intricate cogs and tubes, steampunk pipes, and colorful cobalt energy coursing through them on background of the bustling futuristic city with flying cars during the afternoon with warm lighting. nvinkpunk'))
